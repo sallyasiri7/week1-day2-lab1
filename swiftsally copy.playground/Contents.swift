af4976e2335c9e72f9713b85bb158c6b07475df6
sallyasiri7 <sallywork2022@gmail.com>
2021-09-28 14:31:18 +0300

    day2 lap1

---
+++ swiftsally copy.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+var a = 4
+var b = 2
+
+print (a+b)
+
+var arr :[Int] = [6,5,4,3,2,1]
+
+arr.append(7)
+
+print(arr[3])
+
+arr.remove(at : 1)
+
+var p : Set = [2 , 1 , 5 , 3]
+
+print(p)
+
+var d : String = "sally"
+
+print(d)
+
+
+
+
